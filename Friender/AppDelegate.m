f74faecd3d8120858fb79d3cfb9121e9f4dd22d8
Andrew Morgan <anoadragon453@gmail.com>
2015-05-23 12:45:23 -0700

    Added Parse keys.

--- Friender/AppDelegate.m
+++ Friender/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import <Parse/Parse.h>
 
 @interface AppDelegate ()
 
@@ -16,7 +17,17 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-    // Override point for customization after application launch.
+    // [Optional] Power your app with Local Datastore. For more info, go to
+    // https://parse.com/docs/ios_guide#localdatastore/iOS
+    [Parse enableLocalDatastore];
+    
+    // Initialize Parse.
+    [Parse setApplicationId:@"Ukl0A1x8KpXvbNukisV2QW7X66QrLkgy8n2E6EVa"
+                  clientKey:@"WB6zTfDCv7ixkEh69XzVPI51LfweruKr6dPtm4fu"];
+    
+    // [Optional] Track statistics around application opens.
+    [PFAnalytics trackAppOpenedWithLaunchOptions:launchOptions];
+    
     return YES;
 }
 
