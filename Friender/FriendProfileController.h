861a1e9e4c64d5aa4e86f4ff500bc4cf2ec7bbb6
Abhishyant Khare <abhishyant@Abhishyants-Mac.local>
2015-05-23 23:07:16 -0700

    go to new view when carousel clicked

---
+++ Friender/FriendProfileController.h
@@ -0,0 +1,13 @@
+//
+//  FriendProfileController.h
+//  Friender
+//
+//  Created by Abhishyant Khare on 5/23/15.
+//  Copyright (c) 2015 CoDevelopers LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FriendProfileController : UIViewController
+
+@end
