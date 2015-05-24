ba11d67c54de9dec64a8645598fbfe608eb32bbc
Andrew Morgan <anoadragon453@gmail.com>
2015-05-24 00:58:04 -0700

    - Stuff is broke, don't touch.

--- Friender/CreateEventViewController.m
+++ Friender/CreateEventViewController.m
@@ -41,6 +41,31 @@
     NSString *dateString = [dateFormat stringFromDate:date];
     return dateString;
 }
+
+// Convert to JPEG with 50% quality
+NSData* data = UIImageJPEGRepresentation(imageView.image, 0.5f);
+PFFile *imageFile = [PFFile fileWithName:@"Image.jpg" data:data];
+
+// Save the image to Parse
+
+[imageFile saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+    if (!error) {
+        // The image has now been uploaded to Parse. Associate it with a new object
+        PFObject* newPhotoObject = [PFObject objectWithClassName:@"PhotoObject"];
+        [newPhotoObject setObject:imageFile forKey:@"image"];
+        
+        [newPhotoObject saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+            if (!error) {
+                NSLog(@"Saved");
+            }
+            else{
+                // Error
+                NSLog(@"Error: %@ %@", error, [error userInfo]);
+            }
+        }];
+    }
+}];
+
 - (IBAction)createEventPressed:(id)sender {
     PFObject *event = [PFObject objectWithClassName:@"Event"];
     event[@"date"] = myDate;
